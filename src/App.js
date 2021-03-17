6063fd6f280729b31d0047e0ebaeb5060e3a89d3
Bwardstories <thewardbunch@gmail.com>
2021-03-17 10:52:35 -0400

    message and message list components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { Switch, Route } from "react-router-dom";
-
+import React, { useEffect } from "react";
 import Home from "./views/Home";
 import NotFound from "./views/NotFound";
+import MessageList from "./views/MessageList";
+import { useStore, GET_MESSAGES } from "./store/store";
+import { getMessages } from "./fetchRequests";
 
 function App() {
+  const messageList = useStore(state => state.messageList);
+  const dispatch = useStore(state => state.dispatch);
+
+  useEffect(async () => {
+    const messageData = await getMessages();
+    dispatch({ type: GET_MESSAGES, payload: messageData.messages });
+  });
+
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/messages" component={MessageList}></Route>
         <Route component={NotFound} />
       </Switch>
+      {/* <MessageList /> */}
     </div>
   );
 }
