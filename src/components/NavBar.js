d29bf39c71c706fbd7567d7649ac4542c833d733
Efgon <egonzalezgar@gmail.com>
2021-03-18 10:06:07 -0400

    started NavBar components

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navigation(props) {
+  return (
+    <div className="Navigation">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">My Profile</Link>{" "}
+        </li>
+        <li>
+          <Link to="/contact">Contacts</Link>{" "}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
